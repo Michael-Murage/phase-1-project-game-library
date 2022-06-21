2be3ed6f3fd42fa00ef4169e0ebe40e4d665ef4a
Michael <mickeymurage@gmail.com>
2022-06-21 21:01:29 +0300

    suspended computer mode play due to AJAX vs SJAX issues faced, will instead implement 2 player mode

--- src/tic.js
+++ src/tic.js
@@ -27,7 +27,6 @@ function addTictactoeToPage(){
     startTictactoe()
     for(let i=1; i<=9; i++){
         let box = document.createElement('div')
-        //box.textContent = `${i}`
         box.id = `${i}`
         box.classList.add('box')
         div.appendChild(box)
@@ -48,44 +47,9 @@ function addTictactoeToPage(){
 
 function playTictactoe(b){
     b.addEventListener('click', (e)=>{
-        //first choice
-        let userChoice1 = generateUserChoice(e)
-        const firstChoice = valueId()
-        delete firstChoice[`${userChoice1}`]
-        let compChoice1 = parseFloat(randomise(firstChoice))
-        let compChoice1Elem = document.getElementById(`${compChoice1}`)
-        compChoice1Elem.textContent = 'O'
-        delete firstChoice[`${compChoice1}`]
         
         
-        let userChoice2 = generateUserChoice(e)
-        delete firstChoice[`${userChoice2}`]
-        let compChoice2 = parseFloat(randomise(firstChoice))
-        let compChoice2Elem = document.getElementById(`${compChoice2}`)
-        compChoice2Elem.textContent = 'O'
-        delete firstChoice[`${compChoice2}`]
-
-        let userChoice3 = generateUserChoice(e)
-        delete firstChoice[`${userChoice3}`]
-        let compChoice3 = parseFloat(randomise(firstChoice))
-        let compChoice3Elem = document.getElementById(`${compChoice3}`)
-        compChoice3Elem.textContent = 'O'
-        delete firstChoice[`${compChoice3}`]
-
-        let userChoice4 = generateUserChoice(e)
-        delete firstChoice[`${userChoice4}`]
-        let compChoice4 = parseFloat(randomise(firstChoice))
-        let compChoice4Elem = document.getElementById(`${compChoice4}`)
-        compChoice4Elem.textContent = 'O'
-        delete firstChoice[`${compChoice4}`]
         
-        console.log(firstChoice)
-
-        //console.log(e.target[`${compChoice1}`])
-        // if(parseFloat(e.target.id)){
-        //     // e.target.innerText = 'O'
-        //     console.log(compChoice1, e.target.id)
-        // }
     })
 }
 
@@ -106,56 +70,11 @@ function generateUserChoice(e){
     return parseFloat(e.target.id)
 }
 
-function option(rand){
-    if(rand === 1){
-        one = 'O'
-    }else if(rand === 2){
-        two = 'O'
-    }else if(rand === 3){
-        three = 'O'
-    }else if(rand === 4){
-        four = 'O'
-    }else if(rand === 5){
-        five = 'O'
-    }else if(rand === 6){
-        six = 'O'
-    }else if(rand === 7){
-        seven = 'O'
-    }else if(rand === 8){
-        eight = 'O'
-    }else if(rand === 9){
-        nine = 'O'
-    }
-}
-
-function valueId(){
-    return{
-        1: 'one',
-        2: "two",
-        3: 'three',
-        4: 'four',
-        5: 'five',
-        6: 'six',
-        7: 'seven',
-        8: 'eight',
-        9: 'nine'
-    }
-}
-
-
-function randomiseCompChoice(){
-    //const rand = Math.floor(Math.random() * 9) + 1
-    //first choice
-
-}
-
-function generateCompChoice(){
-    // const rand = Math.floor(Math.random() * 9) + 1
-    // if(rand === 1 && one === ''){
-
-    // }
+function checkWin(){
     
 }
+
+
 
 function stopTictactoe(){
     div.classList.remove('parentGrid')
@@ -166,3 +85,48 @@ function startTictactoe(){
     div.classList.remove('#interface')
     div.classList.add('parentGrid')
 }
+
+
+
+// let userChoice1 = generateUserChoice(e)
+//         const firstChoice = valueId()
+//         delete firstChoice[`${userChoice1}`]
+//         let compChoice1 = parseFloat(randomise(firstChoice))
+//         let compChoice1Elem = document.getElementById(`${compChoice1}`)
+//         compChoice1Elem.textContent = 'O'
+//         delete firstChoice[`${compChoice1}`]
+        
+//         let userChoice2 = generateUserChoice(e)
+//         delete firstChoice[`${userChoice2}`]
+//         let compChoice2 = parseFloat(randomise(firstChoice))
+//         let compChoice2Elem = document.getElementById(`${compChoice2}`)
+//         compChoice2Elem.textContent = 'O'
+//         delete firstChoice[`${compChoice2}`]
+        
+//         let userChoice3 = generateUserChoice(e)
+//         delete firstChoice[`${userChoice3}`]
+//         let compChoice3 = parseFloat(randomise(firstChoice))
+//         let compChoice3Elem = document.getElementById(`${compChoice3}`)
+//         compChoice3Elem.textContent = 'O'
+//         delete firstChoice[`${compChoice3}`]
+
+//         let userChoice4 = generateUserChoice(e)
+//         delete firstChoice[`${userChoice4}`]
+//         let compChoice4 = parseFloat(randomise(firstChoice))
+//         let compChoice4Elem = document.getElementById(`${compChoice4}`)
+//         compChoice4Elem.textContent = 'O'
+//         delete firstChoice[`${compChoice4}`]
+
+// function valueId(){
+//     return{
+//         1: 'one',
+//         2: "two",
+//         3: 'three',
+//         4: 'four',
+//         5: 'five',
+//         6: 'six',
+//         7: 'seven',
+//         8: 'eight',
+//         9: 'nine'
+//     }
+// }
