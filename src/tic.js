dcc76ea5f69815378dd609a2b112e0cf9dfc2134
Michael <mickeymurage@gmail.com>
2022-06-22 11:09:11 +0300

    resurrecting computer play

--- src/tic.js
+++ src/tic.js
@@ -20,6 +20,7 @@ function hideMenu(){
 
 
 let one, two, three, four, five, six, seven, eight, nine;
+let randomArr = [];
 
 tic.addEventListener('click', addTictactoeToPage)
 
@@ -32,26 +33,65 @@ function addTictactoeToPage(){
         div.appendChild(box)
     }
     hideMenu()
-    one = document.getElementById('1').innerHTML
-    two = document.getElementById('2').innerHTML
-    three = document.getElementById('3').innerHTML
-    four = document.getElementById('4').innerHTML
-    five = document.getElementById('5').innerHTML
-    six = document.getElementById('6').innerHTML
-    seven = document.getElementById('7').innerHTML
-    eight = document.getElementById('8').innerHTML
-    nine = document.getElementById('9').innerHTML
+    one = document.getElementById('1')
+    two = document.getElementById('2')
+    three = document.getElementById('3')
+    four = document.getElementById('4')
+    five = document.getElementById('5')
+    six = document.getElementById('6')
+    seven = document.getElementById('7')
+    eight = document.getElementById('8')
+    nine = document.getElementById('9')
+    
     let box = document.querySelectorAll('.box')
-    box.forEach(playTictactoe)
+    // div.addEventListener('click', addPlayer1)
+    // div.addEventListener('click', add)
+    box.forEach(b => b.addEventListener('click', (e)=>{
+        const firstChoice = valueId()
+        let userChoice1 = generateUserChoice(e)
+        
+        if(randomArr.length === 1){
+            delete firstChoice[`${userChoice1}`]
+            let compChoice1 = parseFloat(randomise(firstChoice))
+            let compChoice1Elem = document.getElementById(`${compChoice1}`)
+            compChoice1Elem.textContent = 'O'
+            delete firstChoice[`${compChoice1}`]
+        }
+        
+        
+        let userChoice2 = generateUserChoice(e)
+        console.log(randomArr)
+        if(randomArr.length === 2){
+            delete firstChoice[`${userChoice2}`]
+            let compChoice2 = parseFloat(randomise(firstChoice))
+            let compChoice2Elem = document.getElementById(`${compChoice2}`)
+            compChoice2Elem.textContent = 'O'
+            delete firstChoice[`${compChoice2}`]
+        }
+        
+        let userChoice3 = generateUserChoice(e)
+        console.log(randomArr)
+        if(randomArr.length === 3){
+            
+            delete firstChoice[`${userChoice3}`]
+            let compChoice3 = parseFloat(randomise(firstChoice))
+            let compChoice3Elem = document.getElementById(`${compChoice3}`)
+            compChoice3Elem.textContent = 'O'
+            delete firstChoice[`${compChoice3}`]
+        }
+        let userChoice4 = generateUserChoice(e)
+        
+        if(randomArr.length === 4){
+            
+            delete firstChoice[`${userChoice4}`]
+            let compChoice4 = parseFloat(randomise(firstChoice))
+            let compChoice4Elem = document.getElementById(`${compChoice4}`)
+            compChoice4Elem.textContent = 'O'
+            delete firstChoice[`${compChoice4}`]
+        }
+}))
 }
 
-function playTictactoe(b){
-    b.addEventListener('click', (e)=>{
-        
-        
-        
-    })
-}
 
 function randomise(obj){
     let keys = []
@@ -63,12 +103,33 @@ function randomise(obj){
     return keys[keys.length * Math.random() << 0]
 }
 
+// function addPlayer1(e){
+//     if(e.target.innerText === ''){
+//         e.target.innerText = 'X'
+//     }
+//     console.log(e)
+// }
+
 function generateUserChoice(e){
     if(e.target.innerText === ''){
         e.target.innerText = 'X'
     }
-    return parseFloat(e.target.id)
+    if(e.target.innerText === 'X'){
+        randomArr.push('X')
+    }
 }
+
+// function addPlayer2(e){
+//     if(e.target.innerText === ''){
+//         e.target.innerText = 'O'
+//     }
+// }
+
+// function generateUser2Choice(){
+//     div.addEventListener('click', (e)=>{
+    
+// })
+// }
 
 function checkWin(){
     
@@ -117,16 +178,16 @@ function startTictactoe(){
 //         compChoice4Elem.textContent = 'O'
 //         delete firstChoice[`${compChoice4}`]
 
-// function valueId(){
-//     return{
-//         1: 'one',
-//         2: "two",
-//         3: 'three',
-//         4: 'four',
-//         5: 'five',
-//         6: 'six',
-//         7: 'seven',
-//         8: 'eight',
-//         9: 'nine'
-//     }
-// }
+function valueId(){
+    return{
+        1: 'one',
+        2: "two",
+        3: 'three',
+        4: 'four',
+        5: 'five',
+        6: 'six',
+        7: 'seven',
+        8: 'eight',
+        9: 'nine'
+    }
+}
